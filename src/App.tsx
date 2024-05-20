c5f108b4f27bacfa407b8087d079591a4902abca
iradecimal <iggesmundo@up.edu.ph>
2024-05-20 13:53:20 +0800

    create search ui app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+import React from "react";
+import {
+  ErrorBoundary,
+  Facet,
+  SearchProvider,
+  SearchBox,
+  Results,
+  PagingInfo,
+  ResultsPerPage,
+  Paging,
+  WithSearch
+} from "@elastic/react-search-ui";
+import {
+  BooleanFacet,
+  Layout,
+  SingleLinksFacet,
+  SingleSelectFacet
+} from "@elastic/react-search-ui-views";
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import { SearchDriverOptions } from "@elastic/search-ui";
 
-function App() {
+const connector = new AppSearchAPIConnector({
+  searchKey: "search-ca3hakxq68dfhj5ibnwboutd",
+  engineName: "test",
+  endpointBase: "https://cmsc191.ent.asia-southeast1.gcp.elastic-cloud.com"
+});
+
+const config: SearchDriverOptions = {
+  alwaysSearchOnInitialLoad: true,
+  apiConnector: connector,
+  hasA11yNotifications: true,
+  searchQuery: {
+    result_fields: {
+      title: { raw: {} },
+      authors: { raw: {} },
+      publicationcategory: { raw: {}},
+      publicationyear: { raw: {} },
+      keywords: { raw: {} },
+      isecopyavailable: { raw : {} },
+      doi: { raw: {}},
+      url: { raw: {}},
+      booktitle: { raw: {}},
+      conferencename: {raw: {}},
+      conferencedate: {raw: {}}
+    },
+    search_fields: {
+      title: {},
+      authors: {},
+      publicationcategory: {},
+      publicationyear: {},
+      keywords: {},
+    },
+    disjunctiveFacets: ["publicationyear"],
+    facets: {
+     publicationcategory: { type: "value", size: 20},
+     publicationyear: {type: "value", size: 20},
+     keywords: {type: "value"},
+     isecopyavailable: {type: "value"}
+    }
+  }
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SearchProvider config={config}>
+      <WithSearch
+        mapContextToProps={({ wasSearched }) => ({
+          wasSearched
+        })}
+      >
+        {({ wasSearched }) => {
+          return (
+            <div className="App">
+              <ErrorBoundary>
+                <Layout
+                  header={<SearchBox searchAsYouType={true} debounceLength={300} />}
+                  sideContent={<div>
+                    <Facet
+                      field="publicationcategory"
+                      label="Publication Category"
+                      isFilterable={true}
+                    />
+                    <Facet
+                      field="publicationyear"
+                      label="Publication Year"
+                      filterType="any"
+                      isFilterable={true}
+                    />
+                    <Facet
+                      field="isecopyavailable"
+                      label="E Copy Available"
+                      view={BooleanFacet}
+                      isFilterable={true}
+                    />
+                    <Facet
+                      field="keywords"
+                      label="Keywords"
+                      isFilterable={true}
+                    />
+                  </div>}
+                  bodyContent={
+                    <Results
+                      titleField="title"
+                      urlField="nps_link"
+                      thumbnailField="image_url"
+                      shouldTrackClickThrough
+                    />
+                  }
+                  bodyHeader={
+                    <React.Fragment>
+                      {wasSearched && <PagingInfo />}
+                      {wasSearched && <ResultsPerPage />}
+                    </React.Fragment>
+                  }
+                  bodyFooter={<Paging />}
+                />
+              </ErrorBoundary>
+            </div>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
   );
 }
-
-export default App;
